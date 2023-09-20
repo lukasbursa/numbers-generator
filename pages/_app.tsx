2e813eba71b877d110455dc10d409c83891f6e74
LukasBursa <bursalukas@gmail.com>
2023-09-20 20:42:41 +0200

    update / header file name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../store/index";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "../components/header";
+import Header from "../Components/header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
